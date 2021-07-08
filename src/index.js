58234b270d50f7ddffaee437a8d64ac0d4368d26
Mari Taksa <welch.mari@gmail.com>
2021-07-08 18:31:26 -0500

    commit tree 2957f8bd73c6491f99d82f083f27fe67708ef002

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
+function fetchRegisteredDogs() {
+    fetch ('http://localhost:3000/dogs')
+    .then (resp => resp.json())
+    .then (json => renderRegisteredDogs(json))
+}
 
+function renderRegisteredDogs(dogs) {
+    dogs.forEach(dog => {
+        let tbody = document.querySelector('#table-body')
+        let newDog = document.createElement('tr')
+        newDog.innerHTML = `
+        <tr><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button>Edit</button></td></tr>`
+        tbody.append(newDog)
+    
+    newDog.addEventListener('click', value => {
+        document.querySelectorAll('#dog-form input')[0].value = dog.name
+        document.querySelectorAll('#dog-form input')[1].value = dog.breed
+        document.querySelectorAll('#dog-form input')[2].value = dog.sex  
+    })
+})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchRegisteredDogs()
 })
